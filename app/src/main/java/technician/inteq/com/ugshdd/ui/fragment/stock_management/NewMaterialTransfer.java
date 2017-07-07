3b9a1a6744d5a8eb56eb338642a5db6623b6ca68
NishantSambyal <nishantsambyal123@gmail.com>
2017-07-07 11:17:29 +0530

    successful

---
+++ app/src/main/java/technician/inteq/com/ugshdd/ui/fragment/stock_management/NewMaterialTransfer.java
@@ -0,0 +1,58 @@
+package technician.inteq.com.ugshdd.ui.fragment.stock_management;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import technician.inteq.com.ugshdd.R;
+
+public class NewMaterialTransfer extends Fragment {
+
+
+    public NewMaterialTransfer() {
+        // Required empty public constructor
+    }
+
+    public static NewMaterialTransfer newInstance(String param1, String param2) {
+        NewMaterialTransfer fragment = new NewMaterialTransfer();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_new_material_transfer, container, false);
+        return view;
+    }
+
+    public void onButtonPressed(Uri uri) {
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
--- app/src/main/java/technician/inteq/com/ugshdd/ui/fragment/stock_management/TransferItemFragment.java
+++ app/src/main/java/technician/inteq/com/ugshdd/ui/fragment/stock_management/TransferItemFragment.java
@@ -3,24 +3,34 @@ package technician.inteq.com.ugshdd.ui.fragment.stock_management;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import technician.inteq.com.ugshdd.R;
+import technician.inteq.com.ugshdd.adapters.LeaveRVAdapter;
 
 public class TransferItemFragment extends Fragment {
 
+    private RecyclerView materialListRV;
+    private List<String> materialTransferList;
+    private FloatingActionButton floatingActionButton;
 
     public TransferItemFragment() {
-
     }
 
     public static TransferItemFragment newInstance(String param1, String param2) {
         TransferItemFragment fragment = new TransferItemFragment();
         Bundle args = new Bundle();
-
         fragment.setArguments(args);
         return fragment;
     }
@@ -29,18 +39,37 @@ public class TransferItemFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_transfer_item, container, false);
+        View view = inflater.inflate(R.layout.fragment_transfer_item, container, false);
+        materialListRV = (RecyclerView) view.findViewById(R.id.material_transfer_list);
+        materialTransferList = new ArrayList<>();
+        for (int i = 0; i <= 9; i++) {
+            materialTransferList.add("item " + i);
+        }
+
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(getContext());
+        materialListRV.setLayoutManager(manager);
+        materialListRV.setAdapter(new LeaveRVAdapter(materialTransferList, getContext()));
+        floatingActionButton = (FloatingActionButton) view.findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.replace(R.id.main_layout, new NewMaterialTransfer());
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
 
     }
@@ -48,17 +77,11 @@ public class TransferItemFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
 
-    }
-
-    public interface OnFragmentInteractionListener {
-
-        void onFragmentInteraction(Uri uri);
     }
 }
