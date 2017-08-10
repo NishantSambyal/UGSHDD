7ac314435ccfaa54eb08522390a18d64d0738674
NishantSambyal <nishantsambyal123@gmail.com>
2017-08-10 18:00:32 +0530

    successful

--- app/src/main/java/technician/inteq/com/ugshdd/ui/activity/PDFViewerActivity.java
+++ app/src/main/java/technician/inteq/com/ugshdd/ui/activity/PDFViewerActivity.java
@@ -1,13 +1,9 @@
 package technician.inteq.com.ugshdd.ui.activity;
 
-import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.joanzapata.pdfview.PDFView;
-import com.joanzapata.pdfview.listener.OnDrawListener;
-import com.joanzapata.pdfview.listener.OnLoadCompleteListener;
-import com.joanzapata.pdfview.listener.OnPageChangeListener;
 
 import java.io.File;
 
@@ -26,25 +22,7 @@ public class PDFViewerActivity extends AppCompatActivity {
                 .defaultPage(1)
                 .showMinimap(false)
                 .enableSwipe(true)
-                .onDraw(new OnDrawListener() {
-                    @Override
-                    public void onLayerDrawn(Canvas canvas, float pageWidth, float pageHeight, int displayedPage) {
-
-                    }
-                })
-                .onLoad(new OnLoadCompleteListener() {
-                    @Override
-                    public void loadComplete(int nbPages) {
-
-                    }
-                })
-                .onPageChange(new OnPageChangeListener() {
-                                  @Override
-                                  public void onPageChanged(int page, int pageCount) {
-
-                                  }
-                              }
-                )
+                .swipeVertical(true)
                 .load();
 
     }
